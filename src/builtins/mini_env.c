37f89e3f730d9a26bf70d72496e1cac6b3b550a0
escaleira <129979364+eescaleira@users.noreply.github.com>
2024-07-17 18:57:36 +0100

    mini_env error correction

--- src/builtins/mini_env.c
+++ src/builtins/mini_env.c
@@ -6,7 +6,7 @@
 /*   By: eescalei <eescalei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 17:58:46 by eescalei          #+#    #+#             */
-/*   Updated: 2024/07/17 18:08:59 by eescalei         ###   ########.fr       */
+/*   Updated: 2024/07/17 18:54:17 by eescalei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	mini_env(t_shell *shell)
 	env = shell->ev;
 	while (env)
 	{
+		write(1, env->name, ft_strlen(env->name));
+		write(1, "=", 1);
 		write(1, env->value , ft_strlen(env->value));
 		write(1, "\n", 1);
 		env = env->next;
